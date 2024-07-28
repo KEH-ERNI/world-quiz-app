9259a27e2a4a0e2791df9144f328717ff6eaddee
khylaehza <khondrade@gmail.com>
2024-07-29 00:30:30 +0800

    Added controller for quiz.

---
+++ api/Controllers/QuizController.cs
@@ -0,0 +1,161 @@
+﻿using api.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class QuizController : ControllerBase
+	{
+		private readonly DataContext _context;
+		private readonly IMapper _mapper;
+		private readonly IWebHostEnvironment _webHostEnvironment;
+
+		public QuizController(DataContext context, IMapper mapper, IWebHostEnvironment webHostEnvironment )
+		{
+			_context = context;
+			_mapper = mapper;
+			_webHostEnvironment = webHostEnvironment;
+		}
+
+		[HttpGet]
+		public IActionResult Get() { }
+
+		/*using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Threading.Tasks;
+using YourNamespace.Models;
+using YourNamespace.Services;
+
+[ApiController]
+[Route("api/[controller]")]
+public class QuizController : ControllerBase
+{
+    private readonly DataContext _context;
+    private readonly IMapper _mapper;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public QuizController(DataContext context, IMapper mapper, IWebHostEnvironment webHostEnvironment)
+    {
+        _context = context;
+        _mapper = mapper;
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<QuizDto>>> GetQuizzes()
+    {
+        var quizzes = await _context.Quizzes.ToListAsync();
+        return Ok(_mapper.Map<IEnumerable<QuizDto>>(quizzes));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<QuizDto>> GetQuiz(int id)
+    {
+        var quiz = await _context.Quizzes.FindAsync(id);
+
+        if (quiz == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(_mapper.Map<QuizDto>(quiz));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<QuizDto>> CreateQuiz([FromForm] QuizDto quizDto)
+    {
+        var quiz = _mapper.Map<Quiz>(quizDto);
+
+        if (quizDto.ImageFile != null)
+        {
+            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", quizDto.ImageFile.FileName);
+            using (var stream = new FileStream(imagePath, FileMode.Create))
+            {
+                await quizDto.ImageFile.CopyToAsync(stream);
+            }
+            quiz.ImageName = quizDto.ImageFile.FileName;
+        }
+
+        _context.Quizzes.Add(quiz);
+        await _context.SaveChangesAsync();
+
+        var createdQuiz = _mapper.Map<QuizDto>(quiz);
+        return CreatedAtAction(nameof(GetQuiz), new { id = quiz.QuizID }, createdQuiz);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateQuiz(int id, [FromForm] QuizDto quizDto)
+    {
+        if (id != quizDto.QuizID)
+        {
+            return BadRequest();
+        }
+
+        var quiz = await _context.Quizzes.FindAsync(id);
+
+        if (quiz == null)
+        {
+            return NotFound();
+        }
+
+        if (quizDto.ImageFile != null)
+        {
+            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", quizDto.ImageFile.FileName);
+            using (var stream = new FileStream(imagePath, FileMode.Create))
+            {
+                await quizDto.ImageFile.CopyToAsync(stream);
+            }
+            quiz.ImageName = quizDto.ImageFile.FileName;
+        }
+
+        _mapper.Map(quizDto, quiz);
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!QuizExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteQuiz(int id)
+    {
+        var quiz = await _context.Quizzes.FindAsync(id);
+        if (quiz == null)
+        {
+            return NotFound();
+        }
+
+        _context.Quizzes.Remove(quiz);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool QuizExists(int id)
+    {
+        return _context.Quizzes.Any(e => e.QuizID == id);
+    }
+}
+
+		 */
+	}
+}
---
+++ api/Dto/QuizDto.cs
@@ -0,0 +1,36 @@
+﻿using api.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Dto
+{
+	public class QuizDto
+	{
+		[Key]
+		public int QuizID { get; set; }
+
+		public int UserID { get; set; }
+
+		[MaxLength(50)]
+		public string Name { get; set; } = string.Empty;
+
+		[MaxLength(50)]
+		public string Category { get; set; } = "Multiple Choice";
+
+		[MaxLength(50)]
+		public string Difficulty { get; set; } = "Easy";
+
+		public int Quantity { get; set; } = 10;
+
+		[MaxLength(255)]
+		public string Description { get; set; } = string.Empty;
+
+		public IFormFile ImageFile { get; set; }
+
+		public int Code { get; set; }
+
+		public ICollection<Question>? Questions { get; set; }
+
+		public ICollection<TakeQuiz>? TakeQuizzes { get; set; }
+	}
+}
